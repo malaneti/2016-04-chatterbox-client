6e6eaa3a0d973704ddf5ac8f88691222aea2daf6
Hack Reactor Students <accounts@hackreactor.com>
2016-05-02 19:08:29 -0700

    Made progress on app and chatroom behavior

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,79 +1,84 @@
 // YOUR CODE HERE:
-
-$(document).ready(function(){
-
+//$(document).ready(function() {
 var app = {
-	server: 'https://api.parse.com/1/classes/chatterbox',
-	init: function(){
-		app.fetch();
-		app.addFriend();
-		app.addRoom();
-		app.addMessage();
+  rooms: [],
+  chats: {},
+  server: 'https://api.parse.com/1/classes/messages',
+  init: function() {
+    // app.fetch(server);
+		// app.addFriend();
+		// app.addRoom();
+    // app.addMessage();
+    app.clearMessages();
+  },
 
-	},
-	send: function(message){
-	$.ajax({
-		url: app.server,
-		type: "POST",
-		data:JSON.stringify(message),
-		contentType: "applicatoin/json",
-		success: function(data){
-			console.log('chatterbox: Message sent')
-		}
-		error: function(data){
-			console.log("chatterbox: Failed to send message")
-		}
+  send: function(message) {
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      text: message,
+      contentType: 'application/json',
+      success: function(data) {
+        console.log(data);
+        // app.fetch(server);
+      },
+      error: function(data) {
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },  //end of send
+  fetch: function(message) {
+    console.log('hi');
+    $.ajax({
+      url: app.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+				//app.clearMessages();
+        chats = data;
+        console.log(chats);
+        //addMessage(data);
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
 
-	})
-	},  //end of send 
-	fetch: function(message){
-		$.ajax({
-			url: app.server,
-			type: "GET",
-			data: 
-			contentType: "application/json"
-			success: function(data){
-
-			}
-		    error: function (data) {
-          	console.error('chatterbox: Failed to send message');
-          }
-		})
-	},
-
-	submitMessage: function(){
-
-	},
-
-	addFriend: function(){
-
-	},
-
-	addRoom: function (){
-
-	},
+  addMessage: function(message, room) {
+    
 
 
+    //For appending to DOM only
+    //Does not call send
+    if (app.rooms.indexOf(room) === -1) {
+      addRoom(room);
+    }
+    // add room as class for each msg
+  },
 
-	
+  clearMessages: function() {
+    var $childArr = $('#chats').children();
+    for (var i = 0; i < $childArr.length; i++) {
+      $childArr[i].remove();
+    }
+  },
 
+  addFriend: function() {
 
+  },
 
+  addRoom: function (room) {
+    
+  },
 
+  handleSubmit: function(msg) {
+    app.send({
+      text: msg,
+      username: username
+    });
+  }
 
-
-
-
-
-		}
-
-
-
-
-
-
-
-
-
-
-})
+};
+ //});
